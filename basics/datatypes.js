0af5877cc5973da952d61b50c1d0a76a3f81efb8
Ritesh Patil <ritessshhh7@gmail.com>
2024-09-11 21:33:15 +0530

    Setup and basic datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,12 @@
+/* Boolean */
+// let flag: boolean;
+// flag = true;
+// console.log(true);
+// if (!flag)
+//     console.log("false")
+// else
+//     console.log("true");
+/* Numbers */
+var a = 23.09;
+var b = 9007199254740991n;
+console.log(a, b);
---
+++ basics/datatypes.ts
@@ -0,0 +1,17 @@
+/* Boolean */
+
+// let flag: boolean;
+// flag = true;
+
+// console.log(true);
+
+// if (!flag)
+//     console.log("false")
+// else
+//     console.log("true");
+
+/* Numbers */
+
+let a: number = 23.09;
+let b: bigint = 9007199254740991n;
+console.log(a, b);
