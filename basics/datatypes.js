a20004afbaf66e8574e93fb5ee717ada834f1fc0
Ritesh Patil <ritessshhh7@gmail.com>
2024-09-13 20:14:11 +0530

    basic datatypes

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -7,6 +7,34 @@
 // else
 //     console.log("true");
 /* Numbers */
-var a = 23.09;
-var b = 9007199254740991n;
-console.log(a, b);
+// let a: number = 23.09;
+// let b: bigint = 9007199254740991n;
+// console.log(a, b);
+// let c: number
+// // c = "Hello"; //this gives error compile time
+// console.log(c);
+/* Arrays */
+// let nums: number[];
+// nums = [10, 20, 30];
+// console.log(nums);
+// let names: string[];
+// names = ['mahesh', 'ritesh', 'sanket'];
+// console.log(names);
+/* Objects */
+// let employee: {
+//     firstName: string;
+//     lastName: string;
+//     age: number;
+//     jobTitle: string;
+// };
+// employee = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     age: 25,
+//     jobTitle: 'Web Developer'
+// };
+// console.log(employee);
+var fname = "ritesh patil";
+var role = "pda";
+var profile = "My name is ".concat(fname, ",\n    and my role is ").concat(role);
+console.log(profile);
--- basics/datatypes.ts
+++ basics/datatypes.ts
@@ -12,6 +12,47 @@
 
 /* Numbers */
 
-let a: number = 23.09;
-let b: bigint = 9007199254740991n;
-console.log(a, b);
+// let a: number = 23.09;
+// let b: bigint = 9007199254740991n;
+// console.log(a, b);
+// let c: number
+// // c = "Hello"; //this gives error compile time
+// console.log(c);
+
+/* Arrays */
+
+// let nums: number[];
+// nums = [10, 20, 30];
+// console.log(nums);
+
+// let names: string[];
+// names = ['mahesh', 'ritesh', 'sanket'];
+// console.log(names);
+
+/* Objects */
+
+// let employee: {
+//     firstName: string;
+//     lastName: string;
+//     age: number;
+//     jobTitle: string;
+// };
+
+// employee = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     age: 25,
+//     jobTitle: 'Web Developer'
+// };
+// console.log(employee);
+
+
+/* STRINGS */
+
+// let fname: string = "ritesh patil";
+// let role: string = "pda";
+
+// let profile = `My name is ${fname},
+//     and my role is ${role}`
+
+// console.log(profile);
